dd5363e27b7a2e16cd4e12c4fc562d28ffe2a272
gagolews <m.gagolewski@gmail.com>
2021-06-01 15:00:07 +1000

    more

--- R/tools.R
+++ R/tools.R
@@ -132,6 +132,8 @@ source2 <- function(file, local=FALSE)
 #'
 #' @return List of all test results, see \code{\link{E}}
 #'
+#' @seealso \code{\link{source2}}
+#'
 #' @export
 test_dir <- function(
     path="tests",
@@ -139,7 +141,7 @@ test_dir <- function(
     recursive=FALSE,
     ignore.case=FALSE
 ) {
-    ..old.realtest_postprocessor = getOption("realtest_postprocessor")
+    ..old.realtest_postprocessor <- getOption("realtest_postprocessor")
     on.exit(options(realtest_postprocessor=..old.realtest_postprocessor))
 
     results_gather <- function(r)
@@ -181,6 +183,44 @@ test_dir <- function(
     results <- .GlobalEnv[["__REALTEST_RESULTS"]]
     .GlobalEnv[["__REALTEST_RESULTS"]] <- NULL
     results
+}
+
+
+#' @export
+results_summarise <- function(results, label_pass="pass", label_fail="fail")
+{
+    s <- lapply(results, function(r) {
+        if (length(r[["matches"]]) == 0)
+            match_name <- label_fail
+        else if (is.null(names(r[["matches"]])))
+            match_name <- label_pass
+        else if (names(r[["matches"]])[1] == "")
+            match_name <- label_pass
+        else
+            match_name <- names(r[["matches"]])[1]
+
+#         avoid_null <- function(x, other) if (is.null(x)) other else x
+
+        c(
+            list(
+                call=deparse(r[["object"]][["expr"]][[1]]),
+                match=match_name
+            ),
+            r[startsWith(names(r), ".")]
+        )
+    })
+
+    common_labels <- unique(unlist(lapply(s, names)))
+    s <- lapply(s, function(r) {
+        for (cl in common_labels)
+            if (is.null(r[[cl]])) r[[cl]] <- NA
+        r
+    })
+
+    s <- do.call(rbind.data.frame, s)
+    s[["match"]] <- factor(s[["match"]],
+        levels=c(setdiff(s[["match"]], c(label_pass, label_fail)), c(label_pass, label_fail)))
+    s
 }
 
 
--- devel/realtest.R
+++ devel/realtest.R
@@ -4,42 +4,6 @@ library("realtest")
 # Sys.setenv("__REALTEST_TEST_FAILURES"="1")
 
 
-results_summarise <- function(results, label_pass="pass", label_fail="fail")
-{
-    s <- lapply(results, function(r) {
-        if (length(r[["matches"]]) == 0)
-            match_name <- label_fail
-        else if (is.null(names(r[["matches"]])))
-            match_name <- label_pass
-        else if (names(r[["matches"]])[1] == "")
-            match_name <- label_pass
-        else
-            match_name <- names(r[["matches"]])[1]
-
-#         avoid_null <- function(x, other) if (is.null(x)) other else x
-
-        c(
-            list(
-                call=deparse(r[["object"]][["expr"]][[1]]),
-                match=match_name
-            ),
-            r[startsWith(names(r), ".")]
-        )
-    })
-
-    common_labels <- unique(unlist(lapply(s, names)))
-    s <- lapply(s, function(r) {
-        for (cl in common_labels)
-            if (is.null(r[[cl]])) r[[cl]] <- NA
-        r
-    })
-
-    s <- do.call(rbind.data.frame, s)
-    s[["match"]] <- factor(s[["match"]],
-        levels=c(setdiff(s[["match"]], c(label_pass, label_fail)), c(label_pass, label_fail)))
-    s
-}
-
 
 r <- test_dir("tests")
 s <- results_summarise(r)
